235081bf568084216636ae1058489e13e37ae9fc
hp_huang <hp_huang@htc.com>
2014-07-22 15:50:19 +0800

    del t3_hotfix on issue11 branch

--- src/com/example/setalarm/MainActivity.java
+++ src/com/example/setalarm/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends Activity implements OnClickListener{
 	TextView t1;
 	TextView t2;
 	TextView t3_hotfix;
-	TextView t4_hotfix;
 	TextView t5;
 	int period = 30;
 	int count = 0;
