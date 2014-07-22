2a350f044af7f6d8719ae81d4b9023b2f8c37075
hp_huang <hp_huang@htc.com>
2014-07-22 15:48:20 +0800

    del t5 at terrible branch

--- src/com/example/setalarm/MainActivity.java
+++ src/com/example/setalarm/MainActivity.java
@@ -37,7 +37,6 @@ public class MainActivity extends Activity implements OnClickListener{
 	TextView t2;
 	TextView t3_hotfix;
 	TextView t4_hotfix;
-	TextView t5;
 	int period = 30;
 	int count = 0;
 	BroadcastReceiver receiver;
